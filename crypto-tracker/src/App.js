a1229a204d634ddbfc2b346e9f1ebfd1a8f52311
koushith <koushith97@gmail.com>
2020-10-01 20:15:40 +0530

    adds axios

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Coin from './Coin';
 import './App.css';
 
+let URL =
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => {
+        console.log(res);
+        setCoins(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handeChange = (e) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a Currency</h1>
+        <form>
+          <input
+            type='text'
+            placeholder='search'
+            className='coin-input'
+            onChange={handeChange}
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
     </div>
   );
 }
 
 export default App;
+
+// []- if you didnt add this, this will call the ULR infinite time. if you add it will run only once
